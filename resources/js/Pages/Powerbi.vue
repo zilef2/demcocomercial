ddd006ab75893a34e729c90a34076691c08e0957
Alejo Madrid <ajelof2@gmail.com>
2025-05-22 18:58:35 -0500

    index-oferta mejorada4

--- resources/js/Pages/Powerbi.vue
+++ resources/js/Pages/Powerbi.vue
@@ -17,11 +17,13 @@ const widthAndHeight =[1940, 1240];
 </script>
 
 <template>
-    <h2>Última actualización: 22 de mayo 2025</h2>
-    <iframe title="3 Demco Comercial 3" :width="widthAndHeight[0]" :height="widthAndHeight[1]"
+    <h2>Última actualización: 23 de mayo 2025</h2>
+    <iframe title="3 Demco Comercial 3" width="1140" height="541.25" 
             src="https://app.powerbi.com/reportEmbed?reportId=f107a1c3-0e93-483d-b2a9-4a2f0de9817a&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"
+            :width="widthAndHeight[0]" :height="widthAndHeight[1]"
             class="pt-8"
     ></iframe>
+<!--    <iframe title="3 Demco Comercial 3" :width="widthAndHeight[0]" :height="widthAndHeight[1]" src="https://app.powerbi.com/reportEmbed?reportId=f107a1c3-0e93-483d-b2a9-4a2f0de9817a&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true" class="pt-8"></iframe>-->
 <!--        <iframe title="Demco Comercial" width="1940" height="1240" src="https://app.powerbi.com/reportEmbed?reportId=4fe1b4bc-0539-46c6-8492-d2082fa67662&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>-->
 <!--        <iframe title="Demco Comercial" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=4fe1b4bc-0539-46c6-8492-d2082fa67662&autoAuth=true&embeddedDemo=true" frameborder="0" allowFullScreen="true"></iframe>-->
 </template>
