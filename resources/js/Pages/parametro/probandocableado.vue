2f098b1c73789e144c6954af5d713c2b52442e7e
Alejo Madrid <ajelof2@gmail.com>
2025-09-10 12:47:52 -0500

    formula cableado erronea2

--- resources/js/Pages/parametro/probandocableado.vue
+++ resources/js/Pages/parametro/probandocableado.vue
@@ -24,8 +24,8 @@ const data = reactive({
     createOpen: false,
     editOpen: false,
     deleteOpen: false,
-    
-    
+
+
     cantidad: 0,
     metros: 10,
     valor: 1,
@@ -48,28 +48,29 @@ const data = reactive({
 
 function calvalorunidad() {
     nextTick()
-    data.valorunidad =  Math.floor(data.metros * data.valor * data.precioequipo) + 5480.475
+    data.valorunidad = (Math.floor(data.metros * data.valor * data.precioequipo) + 3832.5) * 1.43
     data.total = data.valorunidad * data.cantidad
-    
-    
+
+
 }
+
 function calcularCalibre(amperaje) {
-  if (amperaje <= 25) return 12;
-  if (amperaje <= 35) return 10;
-  if (amperaje <= 50) return 8;
-  if (amperaje <= 65) return 6;
-  if (amperaje <= 85) return 4;
-  if (amperaje <= 115) return 2;
-  return null; // fuera de rango
+    if (amperaje <= 25) return 12;
+    if (amperaje <= 35) return 10;
+    if (amperaje <= 50) return 8;
+    if (amperaje <= 65) return 6;
+    if (amperaje <= 85) return 4;
+    if (amperaje <= 115) return 2;
+    return null; // fuera de rango
 }
 
 
 watch([
-          () => data.cantidad,
-          () => data.metros,
-          () => data.valor, 
-          () => data.corriente, 
-      ], () => {
+    () => data.cantidad,
+    () => data.metros,
+    () => data.valor,
+    () => data.corriente,
+], () => {
     calvalorunidad()
 })
 
@@ -85,64 +86,69 @@ watch(() => data.corriente, (new_corriente) => {
                 <main class="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-40 flex flex-1 py-8">
                     <div class="flex w-full max-w-5xl">
                         <div class="mb-8">
-                            <h1 class="text-slate-800 text-3xl sm:text-4xl font-bold tracking-tight"> CABLEADO DE POTENCIA </h1>
-<!--                            <p class="text-sm mt-4"> (CABLE THHN/THWN 12 600V NEGRO)</p>-->
+                            <h1 class="text-slate-800 text-3xl sm:text-4xl font-bold tracking-tight"> Dudas de la
+                                formula CABLEADO DE POTENCIA </h1>
+                            <p class="text-md mt-1"> solo para el ejemplo de calibre 6</p>
+                            <p class="text-sm mt-1"> 1.1 y 1.3 ?? </p>
                         </div>
                     </div>
                 </main>
+            </div>
+
+            <div class="grid grid-cols-2 px-0 py-2 text-center w-full gap-2">
+                <div class="px-2">
+                    <label for="precioequipo" class="w-full">Precio equipo: (CABLE THHN/THWN 12 600V NEGRO)</label>
+                    <p class="w-full text-xs">Se obtiene de una tabla</p>
+                    <input type="number" min=0 v-model="data.precioequipo"
+                           class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+                </div>
+            </div>
+
+            <div class="grid grid-cols-6 px-0 py-2 text-center w-full gap-2">
+                <div class="px-2">
+                    <label for="cantidad">cantidad</label>
+                    <input type="number" min=0 v-model="data.cantidad"
+                           class="mt-1 max-w-32 pl-3 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+                </div>
+                <div class="px-2">
+                    <label for="metros">metros</label>
+                    <input type="number" min=0 v-model="data.metros"
+                           class="mt-1 max-w-32 pl-3 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+                </div>
+                <div class="px-2">
+                    <label for="valor">valor</label>
+                    <input type="number" min=0 v-model="data.valor"
+                           class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
                 </div>
 
-                <div class="grid grid-cols-2 px-0 py-2 text-center w-full gap-2">
-                    <div class="px-2">
-                        <label for="precioequipo" class="w-full">Precio equipo: (CABLE THHN/THWN 12 600V NEGRO)</label>
-                        <p class="w-full text-xs">Se obtiene de una tabla</p>
-                        <input type="number" min=0 v-model="data.precioequipo"
-                            class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
+                <div class="px-2">
+                    <label for="corriente">corriente</label>
+                    <input type="number" min=0 v-model="data.corriente"
+                           class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
                 </div>
-            
-                <div class="grid grid-cols-6 px-0 py-2 text-center w-full gap-2">
-                    <div class="px-2">
-                        <label for="cantidad">cantidad</label>
-                        <input type="number" min=0 v-model="data.cantidad"
-                            class="mt-1 max-w-32 pl-3 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                    <div class="px-2">
-                        <label for="metros">metros</label>
-                        <input type="number" min=0 v-model="data.metros"
-                            class="mt-1 max-w-32 pl-3 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                    <div class="px-2">
-                        <label for="valor">valor</label>
-                        <input type="number" min=0 v-model="data.valor"
-                            class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                    
-                    <div class="px-2">
-                        <label for="corriente">corriente</label>
-                        <input type="number" min=0 v-model="data.corriente"
-                            class="mt-1 max-w-32 pl-3  rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                    <div class="px-2">
-                        <label for="calibre">calibre</label>
-                        <input type="number" min=0 v-model="data.calibre" disabled
-                            class="mt-1 max-w-32 pl-3 bg-gray-400 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                    <div class="px-2">
-                        <label for="valorunidad">valor unidad</label>
-                        <input type="number" min=0 v-model="data.valorunidad" disabled
-                            class="mt-1 max-w-32 pl-3 rounded-md bg-gray-400 mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                <!-- fin grid-->
-                </div>
-                    <div class="px-2 w-full">
-                        <p class="w-full mt-4 text-lg font-bold">Total</p>
-                        <input type="number" min=0 v-model="data.total" disabled
-                            class="mt-1 max-w-32 pl-4 bg-gray-400 text-lg text-red-800 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
-                    </div>
-                
-                
             </div>
+            <div class="grid grid-cols-6 px-0 py-2 text-center w-full gap-2">
+            
+                <div class="px-2">
+                    <label class="capitalize" for="calibre">calibre</label>
+                    <input type="number" min=0 v-model="data.calibre" disabled
+                           class="mt-1 max-w-32 pl-3 bg-gray-400 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+                </div>
+                <div class="px-2">
+                    <label class="capitalize" for="valorunidad">valor unidad</label>
+                    <input type="number" min=0 v-model="data.valorunidad" disabled
+                           class="mt-1 max-w-32 pl-3 rounded-md bg-gray-400 mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+                </div>
+                <!-- fin grid-->
+            </div>
+            <div class="px-2 w-full">
+                <p class="w-full mt-4 text-lg font-bold">Total</p>
+                <input type="number" min=0 v-model="data.total" disabled
+                       class="mt-1 max-w-32 pl-4 bg-gray-400 text-lg text-red-800 rounded-md mx-auto border-[0.5px] border-indigo-200 focus:border-indigo-700"/>
+            </div>
+
+
+        </div>
     </AuthenticatedLayout>
 </template>
 
