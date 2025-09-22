e0b2c92cddf22b5af15e9141410d697eaf8df41d
Alejo Madrid <ajelof2@gmail.com>
2025-09-22 12:49:03 -0500

    vamos realizando ccobre2

--- resources/js/Pages/parametro/ccobre.vue
+++ resources/js/Pages/parametro/ccobre.vue
@@ -41,50 +41,81 @@ const data = reactive({
     total: 461024,
     tiempoTotal: 3.20,
 
-    textocolumna1: ['BARRAJE PRINCIPAL', 'BARRAJE NEUTRO', 'BARRAJE TIERRA','BARRAJE SECUNDARIO 1','BARRAJE SECUNDARIO 2','BARRAJE SECUNDARIO 3','BARRAJE SECUNDARIO 4','BARRAJE SECUNDARIO 5','LAMINILLA'],
+    textocolumna1: ['BARRAJE PRINCIPAL', 'BARRAJE NEUTRO', 'BARRAJE TIERRA', 'BARRAJE SECUNDARIO 1', 'BARRAJE SECUNDARIO 2', 'BARRAJE SECUNDARIO 3', 'BARRAJE SECUNDARIO 4', 'BARRAJE SECUNDARIO 5', 'LAMINILLA'],
+    cantidades: [1, 1, 1, 1],
     amperios: [482, 327, 327, 0], // lo que el usuario digita
     metros: [1.1, 0.4, 0.4, 0],  // el resultado buscado
+    pesos: [1.1, 0.4, 0.4, 0],
+    pesototal: ["0", "0", "0", "0"],
 });
 
 onMounted(() => {
     if (typeof (props.show) === 'undefined') {
         props.show = true;
-
     }
 }) //fin onMounted
 
 console.log("🚀🚀 ~ show: ", props.show);
 const tabla = [
-    {min: 0, max: 273, mts: 1.2},
-    {min: 274, max: 582, mts: 1.4},
-    {min: 583, max: 993, mts: 1.6},
-    {min: 994, max: 1449, mts: 1.8},
-    {min: 1450, max: 1729, mts: 1.8},
-    {min: 1730, max: 2049, mts: 2.0},
-    {min: 2050, max: 3189, mts: 2.2},
-    {min: 3190, max: 4499, mts: 2.4},
-    {min: 4500, max: 5529, mts: 2.4},
-    {min: 5530, max: 6539, mts: 2.6},
-    {min: 6540, max: Infinity, mts: 2.8}
-]
+    {min: 0, max: 161, peso: 0, mts: 0},
+    {min: 162, max: 273, peso: 0.396, mts: 1.2},
+    {min: 274, max: 326, peso: 0.84, mts: 1.4},
+    {min: 327, max: 378, peso: 1.06, mts: 1.4},
+    {min: 379, max: 481, peso: 1.29, mts: 1.4},
+    {min: 482, max: 585, peso: 1.73, mts: 1.6},
+    {min: 586, max: 671, peso: 1.68, mts: 1.6},
+    {min: 672, max: 687, peso: 2.57, mts: 1.6},
+    {min: 688, max: 693, peso: 2.62, mts: 1.6},
+    {min: 694, max: 714, peso: 2.65, mts: 1.6},
+    {min: 715, max: 794, peso: 3.37, mts: 1.6},
+    {min: 795, max: 835, peso: 3.19, mts: 1.6},
+    {min: 836, max: 884, peso: 3.46, mts: 1.6},
+    {min: 885, max: 993, peso: 3.51, mts: 1.6},
+    {min: 994, max: 1079, peso: 4.35, mts: 1.8},
+    {min: 1080, max: 1149, peso: 4.4, mts: 1.8},
+    {min: 1150, max: 1259, peso: 5.24, mts: 1.8},
+    {min: 1260, max: 1449, peso: 6.53, mts: 1.8},
+    {min: 1450, max: 1729, peso: 7.02, mts: 2.0},
+    {min: 1730, max: 2049, peso: 8.79, mts: 2.2},
+    {min: 2050, max: 2479, peso: 13.19, mts: 2.2},
+    {min: 2480, max: 2719, peso: 17.4, mts: 2.2},
+    {min: 2720, max: 3189, peso: 14.03, mts: 2.4},
+    {min: 3190, max: 3259, peso: 17.59, mts: 2.4},
+    {min: 3260, max: 3979, peso: 26.1, mts: 2.4},
+    {min: 3980, max: 4499, peso: 34.8, mts: 2.4},
+    {min: 4500, max: 5529, peso: 41.91, mts: 2.6},
+    {min: 5530, max: 6539, peso: 56.13, mts: 2.8},
+    {min: 6540, max: Infinity, peso: 70.36, mts: 2.8}
+];
 
 const getMts = (valor) => {
     const rango = tabla.find(r => valor >= r.min && valor <= r.max)
-    return rango ? rango.mts : 0
+    return rango ? [rango.mts, rango.peso] : [0, 0]
 }
 
-// const data = reactive({
-//     entrada: null, // lo que el usuario digita
-//     metros: null,  // el resultado buscado
-// });
+const handleAmperiosChange = (valor, idx) => {
+    // Actualiza primero el valor en el array reactivo
+    data.amperios[idx] = Number(valor);
+
+    // Luego, realiza los cálculos con el valor recién actualizado
+    const amp = data.amperios[idx];
+    
+    [data.metros[idx], data.pesos[idx]] = getMts(amp);
+    data.pesototal[idx] = (data.pesos[idx] * data.cantidades[idx]).toFixed(2);
+};
+
+const handlecantidadesChange = (valor, idx) => {
+    data.cantidades[idx] = Number(valor);
+    const canti = data.cantidades[idx];
+    
+    data.pesototal[idx] = (data.pesos[idx] * canti).toFixed(2);
+};
 
 
-watch(() => data.amperios, (amperios) => {
-    amperios.forEach((amp, idx) => {
-        data.metros[idx] = getMts(amp)
-    })
-},{ deep: true});
-
+setTimeout(() => {
+    data.amperios[0] = 5350;
+    console.log('Amperios cambiados a 5350. Espera un segundo...');
+}, 1800);
 
 const confirmFunction = () => {
     const result = parseFloat(data.metros);
@@ -160,26 +191,38 @@ const closeModal = () => {
                         <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
                             <input
                                 type="number"
-                                v-model="data.amperios[index]"
-                                class="w-full pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
+                                :value="data.amperios[index]"
+                                @input="handleAmperiosChange($event.target.value, index)"
+                                class="w-5/6 pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
                             />
-                        </td>
-                        <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
-                            {{ data.barrajePrincipalCant }}
                         </td>
                         <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
                             <input
                                 type="number"
-                                v-model="data.metros[index]"
-                                class="w-full pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
+                                :value="data.cantidades[index]"
+                                @input="handlecantidadesChange($event.target.value, index)"
+                                class="w-5/6 pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
                             />
                         </td>
                         <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
-                            {{ data.barrajePrincipalPesoUnd }}
+                            <input
+                                type="number"
+                                v-model="data.metros[index]" 
+                                class="w-5/6 pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
+                            /> mts
+                        </td>
+                        <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
+                            <input
+                                type="number"
+                                v-model="data.pesos[index]"
+                                class="w-5/6 pl-3 py-2 rounded-md border border-indigo-200 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600"
+                            /> kg
                         </td>
                         <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500 bg-yellow-100">
-                            {{ data.barrajePrincipalPesoTotal }}
+                            {{ data.pesototal[index] }} kg
                         </td>
+
+
                         <td class="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
                             ${{ data.barrajePrincipalValor.toLocaleString() }}
                         </td>
